aaa731e447ac19ccf82c7fdd91c3f507f0abab72
Vishrut Shah <vissha@microsoft.com>
2017-05-24 21:05:47 -0700

    Now

--- scripts/momentOfTruth.js
+++ scripts/momentOfTruth.js
@@ -7,7 +7,7 @@ const exec = require('child_process').exec,
     execSync = require('child_process').execSync,
     util = require('util'),
     utils = require('../test/util/utils'),
-    _ = require('lodash');
+    _ = require('underscore');
 
 /**
  * @class
@@ -16,6 +16,7 @@ class MomentOfTruth {
     constructor(options) {
         this.linterCmd = `autorest --azure-arm=true --message-format=json --input-file=`;
     }
+
 
     getLinterResult(swaggerPath) {
         if (swaggerPath === null || swaggerPath === undefined || typeof swaggerPath.valueOf() !== 'string' || !swaggerPath.trim().length) {
@@ -50,31 +51,76 @@ class MomentOfTruth {
 }
 
 module.exports = MomentOfTruth;
+
+//executes promises sequentially by chaining them.
+function executePromisesSequentially(promiseFactories) {
+    let result = Promise.resolve();
+    promiseFactories.forEach(function (promiseFactory) {
+        result = result.then(promiseFactory);
+    });
+    return result;
+};
+
 let samerIdea = new MomentOfTruth();
 let swaggersToProcess = utils.getFilesChangedInPR();
 let targetBranch = utils.getTargetBranch();
 let sourceBranch = utils.getSourceBranch();
 
-// swaggerToProcess = ['/Users/vishrut/git-repos/rest-repo-reorg/azure-rest-api-specs/arm-storage/2016-01-01/swagger/storage.json'];
-_(swaggersToProcess).each(function (swagger) {
-    samerIdea.getLinterResult(swagger).then((result) => {
-        console.log(result);
+// swaggersToProcess = ['/Users/vishrut/git-repos/rest-repo-reorg/azure-rest-api-specs/arm-storage/2016-01-01/swagger/storage.json'];
+console.log(swaggersToProcess);
+
+// samerIdea.getLinterResult(swaggersToProcess[0]).then((result) => {
+//     console.log(result);
+// });
+
+function runTools(swagger) {
+    console.log(`Processing "${swagger}":`);
+    return samerIdea.getLinterResult(swagger).then(function (linterErrors) {
+        console.log(linterErrors);
+        return;
     });
-}).then(() => {
-    // Checkout the target branch
-    try {
-        console.log(`targetBranch = ${targetBranch}`);
-        console.log(execSync(`git checkout ${targetBranch}`, { encoding: 'utf8' }));
-        console.log(execSync('git branch', { encoding: 'utf8' }));
-        console.log(execSync('git log -2', { encoding: 'utf8' }));
-    } catch (err) {
-        console.log(`An error occurred while getting the current branch ${util.inspect(err, { depth: null })}.`);
-    }
-}).then(() => {
-    console.log(`>>>>>>>>>>>>>>>>>>Do it again on the target branch files>>>>>>>>>>>>>>>>>>`);
-    _(swaggersToProcess).each(function (swagger) {
-        samerIdea.getLinterResult(swagger).then((result) => {
-            console.log(result);
-        });
+}
+
+//main function
+function runScript() {
+    let promiseFactories = _(swaggersToProcess).map(function (swagger) {
+        return function () { return runTools(swagger); };
     });
-});
+    executePromisesSequentially(promiseFactories).then(() => {
+        execSync(`git checkout ${targetBranch}`, { encoding: 'utf8' });
+        // execSync(`git branch`, { encoding: 'utf8' });
+        return executePromisesSequentially(promiseFactories);
+    });
+}
+
+runScript();
+
+
+
+// let promiseFactories = _(swaggersToProcess).map(function (swagger) {
+//     return function () {
+//         return samerIdea.getLinterResult(swagger).then((result) => {
+//             console.log(result);
+//         });
+//     };
+// });
+
+// executePromisesSequentially(promiseFactories);
+
+
+// // Checkout the target branch
+// try {
+//     // console.log(`targetBranch = ${targetBranch}`);
+//     // console.log(execSync(`git checkout ${targetBranch}`, { encoding: 'utf8' }));
+//     // console.log(execSync('git branch', { encoding: 'utf8' }));
+//     // console.log(execSync('git log -2', { encoding: 'utf8' }));
+// } catch (err) {
+//     console.log(`An error occurred while getting the current branch ${util.inspect(err, { depth: null })}.`);
+// }
+
+// console.log(`>>>>>>>>>>>>>>>>>>Do it again on the target branch files>>>>>>>>>>>>>>>>>>`);
+// _(swaggersToProcess).each(function (swagger) {
+//     samerIdea.getLinterResult(swagger).then((result) => {
+//         console.log(result);
+//     });
+// });
