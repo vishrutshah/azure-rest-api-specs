f1b5b959e4b7bb76c4c0f368e94844d339858c03
Vishrut Shah <vissha@microsoft.com>
2017-05-24 21:32:40 -0700

    PR only

--- scripts/momentOfTruth.js
+++ scripts/momentOfTruth.js
@@ -9,48 +9,12 @@ const exec = require('child_process').exec,
     utils = require('../test/util/utils'),
     _ = require('underscore');
 
-/**
- * @class
- */
-class MomentOfTruth {
-    constructor(options) {
-        this.linterCmd = `autorest --azure-arm=true --message-format=json --input-file=`;
-    }
-
-
-    getLinterResult(swaggerPath) {
-        if (swaggerPath === null || swaggerPath === undefined || typeof swaggerPath.valueOf() !== 'string' || !swaggerPath.trim().length) {
-            throw new Error('swaggerPath is a required parameter of type "string" and it cannot be an empty string.');
-        }
-
-        let self = this;
-        let cmd = self.linterCmd + swaggerPath;
-
-        return new Promise((result) => {
-            exec(cmd, { encoding: 'utf8', maxBuffer: 1024 * 1024 * 64 }, (err, stdout, stderr) => {
-                let jsonResult = [];
-                let resultString = stderr;
-                if (resultString.indexOf('{') !== -1) {
-                    resultString = "[" + resultString.substring(resultString.indexOf('{')).trim().replace(/\}\n\{/g, "},\n{") + "]";
-                    //console.log('>>>>>> Trimmed Result...');
-                    //console.log(resultString);
-                    try {
-                        jsonResult = JSON.parse(resultString);
-                        //console.log('>>>>>> Parsed Result...');
-                        //console.dir(resultObject, {depth: null, colors: true});
-                    } catch (e) {
-                        console.log(`An error occurred while executing JSON.parse() on the linter output for ${swaggerPath}:`);
-                        console.dir(resultString);
-                        console.dir(e, { depth: null, colors: true });
-                    }
-                }
-                result(jsonResult);
-            });
-        });
-    }
-}
-
-module.exports = MomentOfTruth;
+let swaggersToProcess = utils.getFilesChangedInPR();
+let targetBranch = utils.getTargetBranch();
+let sourceBranch = utils.getSourceBranch();
+let linterCmd = `autorest --azure-arm=true --message-format=json --input-file=`;
+let gitCheckoutCmd = `git checkout ${targetBranch}`;
+let gitLogCmd = `git log -3`;
 
 //executes promises sequentially by chaining them.
 function executePromisesSequentially(promiseFactories) {
@@ -61,25 +25,47 @@ function executePromisesSequentially(promiseFactories) {
     return result;
 };
 
-let samerIdea = new MomentOfTruth();
-let swaggersToProcess = utils.getFilesChangedInPR();
-let targetBranch = utils.getTargetBranch();
-let sourceBranch = utils.getSourceBranch();
-
-// swaggersToProcess = ['/Users/vishrut/git-repos/rest-repo-reorg/azure-rest-api-specs/arm-storage/2016-01-01/swagger/storage.json'];
-console.log(swaggersToProcess);
-
-// samerIdea.getLinterResult(swaggersToProcess[0]).then((result) => {
-//     console.log(result);
-// });
-
 function runTools(swagger) {
     console.log(`Processing "${swagger}":`);
-    return samerIdea.getLinterResult(swagger).then(function (linterErrors) {
+    return getLinterResult(swagger).then(function (linterErrors) {
         console.log(linterErrors);
         return;
     });
-}
+};
+
+
+function getLinterResult(swaggerPath) {
+    if (swaggerPath === null || swaggerPath === undefined || typeof swaggerPath.valueOf() !== 'string' || !swaggerPath.trim().length) {
+        throw new Error('swaggerPath is a required parameter of type "string" and it cannot be an empty string.');
+    }
+
+    let self = this;
+    let cmd = self.linterCmd + swaggerPath;
+
+    return new Promise((result) => {
+        exec(cmd, { encoding: 'utf8', maxBuffer: 1024 * 1024 * 64 }, (err, stdout, stderr) => {
+            let jsonResult = [];
+            let resultString = stderr;
+            if (resultString.indexOf('{') !== -1) {
+                resultString = "[" + resultString.substring(resultString.indexOf('{')).trim().replace(/\}\n\{/g, "},\n{") + "]";
+                //console.log('>>>>>> Trimmed Result...');
+                //console.log(resultString);
+                try {
+                    jsonResult = JSON.parse(resultString);
+                    //console.log('>>>>>> Parsed Result...');
+                    //console.dir(resultObject, {depth: null, colors: true});
+                } catch (e) {
+                    console.log(`An error occurred while executing JSON.parse() on the linter output for ${swaggerPath}:`);
+                    console.dir(resultString);
+                    console.dir(e, { depth: null, colors: true });
+                }
+            }
+            result(jsonResult);
+        });
+    });
+};
+
+console.log(swaggersToProcess);
 
 //main function
 function runScript() {
@@ -87,40 +73,10 @@ function runScript() {
         return function () { return runTools(swagger); };
     });
     executePromisesSequentially(promiseFactories).then(() => {
-        execSync(`git checkout ${targetBranch}`, { encoding: 'utf8' });
-        // execSync(`git branch`, { encoding: 'utf8' });
+        execSync(`${gitCheckoutCmd}`, { encoding: 'utf8' });
+        execSync(`${gitLogCmd}`, { encoding: 'utf8' });
         return executePromisesSequentially(promiseFactories);
     });
 }
 
 runScript();
-
-
-
-// let promiseFactories = _(swaggersToProcess).map(function (swagger) {
-//     return function () {
-//         return samerIdea.getLinterResult(swagger).then((result) => {
-//             console.log(result);
-//         });
-//     };
-// });
-
-// executePromisesSequentially(promiseFactories);
-
-
-// // Checkout the target branch
-// try {
-//     // console.log(`targetBranch = ${targetBranch}`);
-//     // console.log(execSync(`git checkout ${targetBranch}`, { encoding: 'utf8' }));
-//     // console.log(execSync('git branch', { encoding: 'utf8' }));
-//     // console.log(execSync('git log -2', { encoding: 'utf8' }));
-// } catch (err) {
-//     console.log(`An error occurred while getting the current branch ${util.inspect(err, { depth: null })}.`);
-// }
-
-// console.log(`>>>>>>>>>>>>>>>>>>Do it again on the target branch files>>>>>>>>>>>>>>>>>>`);
-// _(swaggersToProcess).each(function (swagger) {
-//     samerIdea.getLinterResult(swagger).then((result) => {
-//         console.log(result);
-//     });
-// });
